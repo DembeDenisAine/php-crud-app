134061173be8144ae2aefd76ffdbac706e414b84
DembeDenisAine <dembedenisjb@gmail.com>
2022-02-13 01:55:14 +0300

    intial commit, index, files folder

---
+++ index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="files/bootstrap.min.css" rel="stylesheet" 
+      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+      crossorigin="anonymous">
+
+    <title>Ainedembe Denis, PHP crud App!</title>
+
+    <style>
+        .body-bg{
+            background-color: #e8eaeb!important;
+        }
+    </style>
+  </head>
+  <body class="body-bg" >
+  
+
+    <!-- Option  Bootstrap Bundle, bootstrap, jquery, with Popper -->
+    <script src="files/bootstrap.bundle.min.js" 
+    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+     crossorigin="anonymous"></script>
+    <script src="files/jquery.min.js"></script>
+    <script src="files/popper.min.js"></script>
+    <script src="files/bootstrap.min.js"></script>
+  </body>
+</html>
