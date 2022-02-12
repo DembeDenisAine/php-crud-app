11a3d3e35547c3b2361ff2cb733227f7b0a81e86
DembeDenisAine <dembedenisjb@gmail.com>
2022-02-13 02:04:35 +0300

    created: creation form & table

--- index.php
+++ index.php
@@ -14,10 +14,83 @@
         .body-bg{
             background-color: #e8eaeb!important;
         }
+        .white-bg{
+            background-color: #ffff!important;
+        }
+        .light-blue-bg{
+            background-color: lightblue!important;
+        }
     </style>
   </head>
   <body class="body-bg" >
-  
+    <div>
+        <br>
+    </div>
+    <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-4 white-bg" >
+            <h5>My PHP Crud Application</h5>
+            <br>
+            <span > Create New User </span>
+        </div>
+        <div class="col-md-4 light-blue-bg">
+            Users List
+        </div>
+        <div class="col-md-2" ></div>
+    </div>
+    <div class="row" >
+        <div class="col-md-2" ></div>
+        <div class="col-md-3 white-bg">
+            <form method="POST" action="">
+                <div class="form-group">
+                <br>
+                    <input type= "text" name ="firstname" class="form-control" placeholder= "first name"> 
+                </div>
+                <div class="form-group">
+                    <br>
+                    <input type= "text" name ="lastname" class="form-control" placeholder= "last name">
+                </div>
+                <div class="form-group">
+                <br>
+                    <input type="date" name ="dob" class="form-control datepicker" placeholder="date of birth">
+                </div>
+                <div class="form-group">
+                <br>
+                    <input type="submit" name="save" class="btn btn-primary" value="Save Data"> 
+                </div>
+            </form>
+        </div>
+        <div class="col-md-5 light-blue-bg">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">First Name</th>
+                        <th scope="col">Last Name</th>
+                        <th scope="col">Date of Birth</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                        <tr>
+                            <td scope="row">
+                            </td>
+                            <td>
+                            </td>
+                            <td>
+                            </td>
+                            <td>
+                            </td>
+                            <td>
+                            </td>
+                        </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="col-md-2"></div>
+
+    </div>
+
 
     <!-- Option  Bootstrap Bundle, bootstrap, jquery, with Popper -->
     <script src="files/bootstrap.bundle.min.js" 
