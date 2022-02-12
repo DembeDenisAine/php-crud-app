51fab1cad236d1f269ffd082bea4330dfdb2677e
DembeDenisAine <dembedenisjb@gmail.com>
2022-02-13 02:27:55 +0300

    created update script/query in the process.php

--- process.php
+++ process.php
@@ -15,4 +15,17 @@ $mysqli= new mysqli('localhost', 'root','','crud') or die(mysqli_error($mysqli))
     }
 //end save
 
+//edit a record in the database
+if (isset($_POST['edit'])){
 
+    $fname= $_POST['firstname'];
+    $lname=$_POST['lastname'];
+    $date=$_POST['dob'];
+    $id=$_POST['id'];
+
+    $query = "UPDATE data SET firstname='$fname', lastname='$lname',dob='$date' WHERE id='$id'";
+    $saved = $mysqli->query($query);
+    header("Location: index.php");
+    exit();
+}
+//end edit
